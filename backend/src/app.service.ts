2185cfc90603311abb77ba733227ec1172fef6d5
talhaabbasi <talhaabbasi1997@gmail.com>
2022-03-14 00:05:06 +0500

    [FEAT] Listen to API calls

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { CreateUserRequest } from './dtos/create.user.request.body.dto';
+import { CreateUserEvent } from './events/create.user.event';
 
 @Injectable()
 export class AppService {
   private readonly users: any[] = [];
+
+  constructor(
+    @Inject('COMMUNICATION') private readonly communication: ClientProxy,
+  ) {}
 
   getHello(): string {
     return 'Hello World!';
@@ -11,5 +17,6 @@ export class AppService {
 
   createUser(request: CreateUserRequest) {
     this.users.push(request);
+    this.communication.emit('user_created', new CreateUserEvent(request.email));
   }
 }
--- communication/src/app.service.ts
+++ communication/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './events/create.user.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('handleUserCreated - COMMUNICATIONS', data);
+
+    // Email the user...
   }
 }
