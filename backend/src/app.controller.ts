22e458b6f1ad3e8f6bb3f4d8b7ee6c506121d7a0
talhaabbasi <talhaabbasi1997@gmail.com>
2022-03-13 23:51:58 +0500

    [FEAT] Update backend controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserRequest } from './dtos/create.user.request.body.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createUser(@Body() request: CreateUserRequest) {
+    return this.appService.createUser(request);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserRequest } from './dtos/create.user.request.body.dto';
 
 @Injectable()
 export class AppService {
+  private readonly users: any[] = [];
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createUser(request: CreateUserRequest) {
+    this.users.push(request);
   }
 }
